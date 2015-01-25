051ef03f8ac6bb4c0a07bd2e2795079617885086
Fuma13 <Fuma13@users.noreply.github.com>
2015-01-25 01:40:32 +0100

    Scena Finale

---
+++ Assets/Scripts/EinsteinFinalScene.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EinsteinFinalScene : MonoBehaviour {
+
+    public enum Stati { RICOMPARSA, PARLA_LEI, PARLA_LUI, PARLA_LEI2, IDEONA, NERO, NERO2, FINE };
+    public Stati statoCorrente;
+    public Transform dest;
+    private Vector3 inizio;
+    private float deltaMovimento, tempoMovimento;
+    public GameObject moglie, einstein;
+    private DialogueController dc;
+    public Sprite einsteinIdea, einsteinLampadina;
+    public FadeController sfondoNero;
+    public GameObject idonaText;
+
+	// Use this for initialization
+	void Start () {
+        statoCorrente = Stati.RICOMPARSA;
+        inizio = moglie.transform.position;
+        deltaMovimento = 0.01f;
+        tempoMovimento = 0;
+        dc = GetComponent<DialogueController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        switch(statoCorrente)
+        {
+            case Stati.RICOMPARSA:
+                tempoMovimento += deltaMovimento;
+                moglie.transform.position = Vector3.Lerp(inizio, dest.position, tempoMovimento);
+                if (tempoMovimento >= 1)
+                {
+                    statoCorrente = Stati.PARLA_LEI;
+                    dc.resume();
+                }
+                break;
+            case Stati.PARLA_LEI:
+                if (dc.paused)
+                    statoCorrente = Stati.PARLA_LUI;
+                break;
+            case Stati.PARLA_LUI:
+                einstein.GetComponent<SpriteRenderer>().sprite = einsteinIdea;
+                dc.resume();
+                statoCorrente = Stati.PARLA_LEI2;
+                break;
+            case Stati.PARLA_LEI2:
+                if (dc.paused)
+                {
+                    statoCorrente = Stati.IDEONA;
+                    einstein.GetComponent<SpriteRenderer>().sprite = einsteinLampadina;
+                    dc.resume();
+                }
+                break;
+            case Stati.IDEONA:
+                if(dc.paused)
+                {
+                    statoCorrente = Stati.NERO;
+                }
+                break;
+            case Stati.NERO:
+                sfondoNero.FadeOff();
+                statoCorrente = Stati.NERO2;
+                break;
+            case Stati.NERO2:
+                if (!sfondoNero.enabled)
+                {
+                    einstein.GetComponent<SpriteRenderer>().sortingOrder = 3;
+                    idonaText.SetActive(true);
+                    dc.resume();
+                }
+                break;
+            case Stati.FINE:
+                break;
+        }
+	}
+}
