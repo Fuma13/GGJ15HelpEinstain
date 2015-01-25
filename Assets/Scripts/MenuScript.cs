23383eabdc73ca7076ba623cf6121b4e370de295
Lorenzo <niero.lorenzo@gmail.com>
2015-01-25 10:43:58 +0100

    aggiunto menu principale

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void scenaGalileo(){
+        Application.LoadLevel (1);
+    }
+
+    public void scenaCredits(){
+        Application.LoadLevel (9); 
+    }
+}
