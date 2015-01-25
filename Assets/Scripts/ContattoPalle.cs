f8b6450f9664e1e995dbe43683cc2bf573888cd7
bereuge <bereuge@gmail.com>
2015-01-25 11:31:25 +0100

    Qualche dialogo

--- Assets/Scripts/ContattoPalle.cs
+++ Assets/Scripts/ContattoPalle.cs
@@ -3,16 +3,30 @@ using System.Collections;
 
 public class ContattoPalle : MonoBehaviour {
 
-
     private DialogueController dc;
+    public GameObject obj1;
+    public GameObject obj2;
 
     void Start()
     {
-
         dc = GetComponent<DialogueController>();
     }
 
+    void Update()
+    {
+        if (dc.paused)
+        {
+            obj1.rigidbody2D.gravityScale = 1;
+            obj2.rigidbody2D.gravityScale = 1;
+        }
+    }
+
 	void OnCollisionEnter2D(Collision2D coll)
+    {
+        Invoke("resume", 1);
+    }
+
+    void resume()
     {
         dc.resume();
     }
