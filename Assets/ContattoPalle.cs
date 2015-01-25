cb109e0a35cde448258f42ac0e345ecdb0832582
Fuma13 <Fuma13@users.noreply.github.com>
2015-01-25 10:55:23 +0100

    Sistemazione scene

---
+++ Assets/ContattoPalle.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ContattoPalle : MonoBehaviour {
+
+
+    private DialogueController dc;
+
+    void Start()
+    {
+
+        dc = GetComponent<DialogueController>();
+    }
+
+	void OnCollisionEnter2D(Collision2D coll)
+    {
+        dc.resume();
+    }
+}
