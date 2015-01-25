8f89c297e3796b8a7dbb57a7cbca24cf1e02174e
Fuma13 <Fuma13@users.noreply.github.com>
2015-01-25 14:39:38 +0100

    Audio scena galileo

---
+++ Assets/PlayAudioMasso.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayAudioMasso : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        if (transform.position.x > 0)
+            audio.pan = 1;
+        else
+            audio.pan = -1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(transform.position.y < 0 && !audio.isPlaying)
+        {
+            audio.Play();
+        }
+	
+	}
+}
