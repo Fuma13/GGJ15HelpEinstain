06278399b5ed593bff493e9cffe0d096a1cfef3e
Fuma13 <Fuma13@users.noreply.github.com>
2015-01-25 15:56:12 +0100

    Pregalileo

---
+++ Assets/LancioPallePre.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LancioPallePre : MonoBehaviour {
+
+
+    public GameObject palla1, palla2;
+    public Animator galileo;
+    public DialogueController dc;
+    private bool lanciato;
+	// Use this for initialization
+	void Start () {
+
+        lanciato = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(!lanciato && dc.paused)
+        {
+            lanciato = true;
+            Invoke("LanciaPalla1", 0);
+            Invoke("LanciaPalla2", 0.5f);
+        }
+
+	}
+
+    private void LanciaPalla1()
+    {
+        palla1.GetComponent<SpriteRenderer>().sortingOrder = 1;
+        palla1.rigidbody2D.gravityScale = 1;
+        galileo.SetTrigger("Restart");
+    }
+
+    private void LanciaPalla2()
+    {
+        palla2.GetComponent<SpriteRenderer>().sortingOrder = 1;
+        palla2.rigidbody2D.gravityScale = 1;
+        galileo.SetTrigger("Restart");
+        dc.resume();
+    }
+}
--- Assets/Scripts/CutScenePreGalileo.cs
+++ Assets/Scripts/CutScenePreGalileo.cs
@@ -12,6 +12,10 @@ public class CutScenePreGalileo : MonoBehaviour {
     private PreGalileoState stato;
     private DialogueController dc;
 
+    public GameObject palla1, palla2;
+    public Animator galileo;
+    private bool lanciato;
+
 
 	// Use this for initialization
 	void Start () {
@@ -37,5 +41,29 @@ public class CutScenePreGalileo : MonoBehaviour {
 
             }
         }
+        else
+        {
+            if (!lanciato && dc.paused)
+            {
+                lanciato = true;
+                Invoke("LanciaPalla1", 0);
+                Invoke("LanciaPalla2", 0.5f);
+            }
+        }
 	}
+
+    private void LanciaPalla1()
+    {
+        palla1.GetComponent<SpriteRenderer>().sortingOrder = 1;
+        palla1.rigidbody2D.gravityScale = 1;
+        galileo.SetTrigger("Restart");
+    }
+
+    private void LanciaPalla2()
+    {
+        palla2.GetComponent<SpriteRenderer>().sortingOrder = 1;
+        palla2.rigidbody2D.gravityScale = 1;
+        galileo.SetTrigger("Restart");
+        dc.resume();
+    }
 }
